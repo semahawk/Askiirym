eaf3f9d7c95c0f9d30f45e66afe3a3237fb53897
Szymon Urbaś <szymon.urbas@aol.com>
2012-12-07 21:29:08 +0100

    Add a welcome printf to main.

---
+++ askiirym.c
@@ -0,0 +1,27 @@
+/*
+ * askiirym.c
+ *
+ * Created at:  07.12.2012 19:29:31
+ *
+ * Author:  Szymon Urbas <szymon.urbas@aol.com>
+ *
+ * License: the MIT license
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "askiirym.h"
+
+/*
+ * TODO: Askiirym
+ */
+
+int main(void)
+{
+  printf("Welcome to Askiirym v%s!\n\n", VERSION);
+
+  return 0;
+}
+
---
+++ askiirym.h
@@ -0,0 +1,11 @@
+/*
+ * askiirym.h
+ *
+ */
+
+#ifndef ASKIIRYM_H
+#define ASKIIRYM_H
+
+#define VERSION "0.1.0"
+
+#endif /* ASKIIRYM_H */
