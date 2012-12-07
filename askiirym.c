9e06c93f17d9eb9d85917667ee3998a203a86346
Szymon Urbaś <szymon.urbas@aol.com>
2012-12-07 21:57:10 +0100

    Add a bit of more code, and a Makefile.

--- askiirym.c
+++ askiirym.c
@@ -15,13 +15,47 @@
 #include "askiirym.h"
 
 /*
- * TODO: Askiirym
+ * TODO: o  Askiirym
+ *       o  Hero stats
+ *       o  Loading saves
+ *       o  De/coding saves
  */
 
 int main(void)
 {
-  printf("Welcome to Askiirym v%s!\n\n", VERSION);
+  int action;
+  char yn;
+
+  printf("Welcome, you adverturer, to Askiirym v%s!\n", VERSION);
+  printf("\n");
+  printf("[1] Start a new adventure\n");
+  printf("[2] Load an andventure (not working)\n");
+  printf("\n");
+  printf("What is your action? ");
+  scanf("%d", &action);
+
+  switch (action){
+    case 1: printf("Are you sure? [Yn] ");
+            scanf(" %c", &yn);
+            if (yn == 'n' || yn == 'N'){
+              printf("\nSee you soon, adventurer!\n\n");
+              exit(0);
+            } else {
+              printf("\nA new adventure is about to begin!\n");
+              newGame();
+            }
+            break;
+
+    case 2: printf("\nHey, it's not working, toldya!\n");
+            exit(0);
+  }
 
   return 0;
+}
+
+void newGame(void)
+{
+  printf("That's been your adventure!\n");
+  exit(0);
 }
 
--- askiirym.h
+++ askiirym.h
@@ -8,4 +8,15 @@
 
 #define VERSION "0.1.0"
 
+struct Hero {
+    char name[32];
+    int hp;
+    int defence;
+    int attack;
+    int money;
+    int level;
+};
+
+void newGame(void);
+
 #endif /* ASKIIRYM_H */
