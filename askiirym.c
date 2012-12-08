ff6fab4787a5c6040b11a76b908f404ef959ab42
Szymon Urbaś <szymon.urbas@aol.com>
2012-12-08 11:35:11 +0100

    Ask for and set the players race.

--- askiirym.c
+++ askiirym.c
@@ -11,6 +11,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "askiirym.h"
 
@@ -19,24 +20,29 @@
  *       o  Hero stats
  *       o  Loading saves
  *       o  De/coding saves
+ *       o  Maybe some plot
  */
+
+struct Hero player;
 
 int main(void)
 {
   int action;
   char yn;
 
+  cl();
   printf("Welcome, you adverturer, to Askiirym v%s!\n", VERSION);
   printf("\n");
   printf("[1] Start a new adventure\n");
   printf("[2] Load an andventure (not working)\n");
   printf("\n");
+askforaction:
   printf("What is your action? ");
-  scanf("%d", &action);
+  get("%d", &action);
 
   switch (action){
-    case 1: printf("Are you sure? [Yn] ");
-            scanf(" %c", &yn);
+    case 1: printf("Are you sure? [yn] ");
+            get("%c", &yn);
             switch (yn){
               case 'n':
               case 'N':
@@ -44,13 +50,14 @@ int main(void)
                 exit(0);
               case 'y':
               case 'Y':
-                printf("\nA new adventure is about to begin!\n");
+                cl();
+                printf("A new adventure is just about to begin!\n\n");
                 newGame();
             }
             break;
 
-    case 2: printf("\nHey, it's not working, toldya!\n");
-            exit(0);
+    case 2: printf("\nHey, it's not working, toldya!\n\n");
+            goto askforaction;
   }
 
   return 0;
@@ -58,7 +65,67 @@ int main(void)
 
 void newGame(void)
 {
-  printf("That's been your adventure!\n");
-  exit(0);
+  char yn;
+  int race;
+
+askforname:
+  printf("So, what's your name, adventurer? (%d chars limit): ", MAX_NAME);
+  fgets(player.name, MAX_NAME, stdin);
+  // remove the newline
+  player.name[strlen(player.name)-1] = '\0';
+  printf("%s, is that right? [yn] ", player.name);
+  get("%c", &yn);
+
+  switch (yn){
+    case 'y':
+    case 'Y':
+      break;
+    default: goto askforname;
+  }
+
+  // some initialization
+  player.hp      = 100;
+  player.defense = 10;
+  player.attack  = 10;
+  player.money   = 250;
+  player.level   = 1;
+
+  cl();
+  printf("There we go! Now, what are you, %s?\n", player.name);
+  printf("\n");
+  printf("[1] Human\n");
+  printf("[2] Elf\n");
+  printf("[3] Orc\n");
+  printf("[4] Gnome\n");
+  printf("\n");
+askforrace:
+  printf("What is your choice? [1-4] ");
+  get("%d", &race);
+
+  printf("\n");
+  switch (race){
+    case 1: printf("You seem to be a human, and get an extra money!\n");
+            player.money += 300;
+            player.race = 'h';
+            break;
+
+    case 2: printf("It turned out you're an elf, you are more defensive!\n");
+            player.defense += 3;
+            player.race = 'e';
+            break;
+
+    case 3: printf("You are an orc, you are a strong, very ugly being, you get more attack!\n");
+            player.attack += 3;
+            player.race = 'o';
+            break;
+
+    case 4: printf("It seems you are a gnome, very short creature, but very healthy, you are healthier!\n");
+            player.hp += 60;
+            player.race = 'g';
+            break;
+
+    default: printf("Unknown race [1-4].\n");
+             goto askforrace;
+  }
 }
 
--- askiirym.h
+++ askiirym.h
@@ -8,10 +8,27 @@
 
 #define VERSION "0.1.0"
 
+#define MAX_NAME 32
+
+#define get(s,p) scanf(s,p); getchar()
+
+#if   defined(_WIN64)
+#  define cl() system("cls")
+#elif defined(_WIN32)
+#  define cl() system("cls")
+#elif defined(__linux)
+#  define cl() system("clear")
+#elif defined(__unix)
+#  define cl() system("clear")
+#else
+#  define cl()
+#endif
+
 struct Hero {
-    char name[32];
+    char name[MAX_NAME];
+    char race;
     int hp;
-    int defence;
+    int defense;
     int attack;
     int money;
     int level;
