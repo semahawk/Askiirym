348844e817d279baa26811a1aa0bb83072c63604
Czeslav <grzegorzkow95@gmail.com>
2012-12-09 17:34:18 +0100

    Added question about work & fixed cl() bugs

--- askiirym.c
+++ askiirym.c
@@ -59,6 +59,8 @@ askforaction:
                 cl();
                 printf("A new adventure is just about to begin!\n\n");
                 newGame();
+                break;
+              default:goto askforaction;
             }
             break;
 
@@ -110,22 +112,26 @@ askforrace:
   get("%d", &action);
 
   switch (action){
-    case 1: printf("You seem to be a human, and get +110 to money!\n");
+    case 1: cl();
+            printf("You seem to be a human, and get +110 to money!\n");
             player.money += 110;
             player.race = 'h';
             break;
 
-    case 2: printf("It turned out you're an elf, you get +3 to defense!\n");
+    case 2: cl();
+            printf("It turned out you're an elf, you get +3 to defense!\n");
             player.defense += 3;
             player.race = 'e';
             break;
 
-    case 3: printf("You are an orc, you are a strong, very ugly being, you get +3 to attack!\n");
+    case 3: cl();
+            printf("You are an orc, you are a strong, very ugly being, you get +3 to attack!\n");
             player.attack += 3;
             player.race = 'o';
             break;
 
-    case 4: printf("It seems you are a gnome, very short creature, but very healthy, you are +30 more healthy!\n");
+    case 4: cl();
+            printf("It seems you are a gnome, very short creature, but very healthy, you are +30 more healthy!\n");
             player.maxhp += 30;
             player.hp += 30;
             player.race = 'g';
@@ -147,22 +153,26 @@ askforraiseness:
   printf("Where was it? [1-4] ");
   get("%d", &action);
 
-  cl();
+
   switch (action){
-    case 1: printf("You were raised in a town, you know your way around. You get +100 money!\n");
+    case 1: cl();
+            printf("You were raised in a town, you know your way around. You get +100 money!\n");
             player.money += 100;
             break;
 
-    case 2: printf("You were raised in a village. Life there wasn't too easy. You get +10 health points!\n");
+    case 2: cl();
+            printf("You were raised in a village. Life there wasn't too easy. You get +10 health points!\n");
             player.maxhp += 10;
             player.hp += 10;
             break;
 
-    case 3: printf("You were raised in a forest. You know the ways to survive an a wildness, you get +2 to defense!\n");
+    case 3: cl();
+            printf("You were raised in a forest. You know the ways to survive an a wildness, you get +2 to defense!\n");
             player.defense += 2;
             break;
 
-    case 4: printf("Your father raised you in his workshop. You were ment to be a true man, stinking from a sweat. You get +3 to attack!\n");
+    case 4: cl();
+            printf("Your father raised you in his workshop. You were ment to be a true man, stinking from a sweat. You get +3 to attack!\n");
             player.attack += 3;
             break;
 
@@ -178,48 +188,91 @@ askforraiseness:
          "[3] They lost your trust, because you had stolen money from them?\n"
          "[4] You left home without any help to look for true love?\n\n");
 askforhomeleaving:
+    printf("What is your choice? [1-4] ");
     get("%d", &action);
 
-cl();
 
     switch (action){
-        case 1:printf("Since you were kid you couldnt stop your strenght, and you get +3 to attack!\n");
+        case 1:cl();
+            printf("Since you were kid you couldnt stop your strenght, and you get +3 to attack!\n");
             player.attack+=3;
             break;
-        case 2:printf("You were very smart, and received a scholarship in size of 50 money!\n");
+        case 2:cl();
+            printf("You were very smart, and received a scholarship in size of 50 money!\n");
             player.money+=50;
             break;
-        case 3:printf("You stole your parens money and hide them under stone, so noone found them, but you feel bad because of that. You gain 100 money, but loose 1 attack and 1 defense\n");
+        case 3:cl();
+            printf("You stole your parens money and hide them under stone, so noone found them, but you feel bad because of that. You gain 100 money, but loose 1 attack and 1 defense\n");
             player.money+=100;
             player.attack-=1;
             player.defense-=1;
             break;
-        case 4:printf("You left your home looking for love, but after many weeks you realized true love doesn't exists. You only lost your time.\n");
+        case 4:cl();
+            printf("You left your home looking for love, but after many weeks you realized true love doesn't exists. You only lost your time.\n");
             break;
         default: printf("Unknown option %d [1-4]\n", action);
             goto askforhomeleaving;
     }
-    printf("\nAnd now you are here, between dirty Hunner Sea and high and steep Angin Mountains, heading to mistery Cimarian City. Why are you here?\n"
+
+
+    printf("\nAfter many years you realized, that world is full of dangers and hate. Endless wars destroyed your family land, and you left to Wanzaniia. You found work in:\n\n"
+           "[1] Smithy\n"
+           "[2] Carpentry shop\n"
+           "[3] Shipyard\n"
+           "[4] Potion shop\n\n");
+askforwork:
+    printf("What is your choice? [1-4] ");
+    get("%d", &action);
+
+    switch(action){
+            case 1:cl();
+                printf("In a smithy you understood real men must be strong. You get +3 attack!");
+                player.attack+=3;
+                break;
+            case 2:cl();
+                printf("As carpenter, you need to be very careful if you want to survive, and you gained +2 to defense!");
+                player.defense+=2;
+                break;
+            case 3:cl();
+                printf("In shipyard you invented new way to join ship parts, and earned 300 money because of that!");
+                player.money+=300;
+                break;
+            case 4:cl();
+                printf("In potion shop you got payment in potions, which made you healthier. 20 points healthier.");
+                player.maxhp+=20;
+                player.hp+=20;
+                break;
+            default:printf("Unknown option %d [1-4]\n", action);
+                goto askforwork;
+    }
+
+
+    printf("\n\nAnd now you are here, between dirty Hunner Sea and high and steep Angin Mountains, heading to mistery Cimarian City. Why are you here?\n\n"
            "[1] You met christian monk, who told you to visit this area to clean your soul\n"
            "[2] You got a recommendation to work in nearest iron mine\n"
            "[3] You want to make good deal and earn some money\n"
-           "[4] You are here to learn how to fight\n");
+           "[4] You are here to learn how to fight\n\n");
 askforreason:
+    printf("What is your choice? [1-4] ");
     get("%d",&action);
 
     switch (action){
-        case 1:printf("You cleant your soul and made yoursefl more healthy. You get +20 HP!\n");
+        case 1:cl();
+            printf("You cleant your soul and made yoursefl more healthy. You get +20 HP!\n");
             player.maxhp+=20;
             player.hp+=20;
             break;
-        case 2:printf("By working in mine you are getting stronger, and get extra +2 to attack! You also earned 50 money!\n");
+        case 2:cl();
+            printf("By working in mine you are getting stronger, and get extra +2 to attack! You also earned 50 money!\n");
             player.attack+=2;
             player.money+=50;
             break;
-        case 3:printf("You made it! You sold some trolls skins, and earned 130 money!\n");
+        case 3:cl();
+            printf("You made it! You sold some trolls skins, and earned 130 money!\n");
             player.money+=130;
             break;
-        case 4:printf("After several weeks of training, your attack and defense raised by 2, but you had to pay 100 money for that!\n");
+        case 4:cl();
+            printf("After several weeks of training, your attack and defense raised by 2, but you had to pay 100 money for that!\n");
             player.attack+=2;
             player.defense+=2;
             player.money-=100;
